081245629ac25b03f0788a639ab0bc27753f5a77
Darina Usanova <darinushkaaaa@gmail.com>
2024-09-27 11:16:23 +0200

    init: add noise

--- DataForceSolutions/main.js
+++ DataForceSolutions/main.js
@@ -1,7 +1,6 @@
 gsap.registerPlugin(ScrollTrigger);
 
 //lenis
-
 const lenis = new Lenis();
 lenis.on("scroll", ScrollTrigger.update);
 gsap.ticker.add((time) => {
@@ -9,110 +8,17 @@ gsap.ticker.add((time) => {
 });
 gsap.ticker.lagSmoothing(0);
 
-// cards animation
+// section scroll
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
 
-// const cards = gsap.utils.toArray(".card");
-
-// cards.forEach((card, index) => {
-//   const tween = gsap.to(card, {
-//     scrollTrigger: {
-//       trigger: card,
-//       start: () => "top bottom-=100",
-//       // end: () => "top top+=40",
-//       scrub: true,
-//       markers: true,
-//       invalidateOnRefresh: true,
-//     },
-//     ease: "none",
-//     scale: () => 1 - (cards.length - index) * 0.025,
-//   });
-//   let pinEndValue = index === cards.length - 1 ? ".end-anim" : "max";
-
-//   ScrollTrigger.create({
-//     trigger: card,
-//     start: "top top+=100",
-//     pin: true,
-//     pinSpacing: false,
-//     markers: true,
-//     id: "pin",
-//     end: pinEndValue,
-//     invalidateOnRefresh: true,
-//   });
-// });
-
-// const tl = gsap.timeline({
-//   defaults: {
-//     ease: "none",
-//   },
-//   scrollTrigger: {
-//     trigger: ".big-wr",
-//     start: "top top",
-//     end: `${window.innerHeight * 1} top`,
-//     scrub: true,
-//     pin: true,
-//     markers: true,
-//   },
-// });
-
-// tl.from(".card:not(:first-child)", {
-//   clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)",
-//   duration: 1,
-//   stagger: 2,
-// });
-
-// tl.to(
-//   ".card",
-//   {
-//     y: 0,
-//     duration: 1,
-//     stagger: 2,
-//   },
-//   "<"
-// );
-
-// cards.forEach((e, i) => {
-//   const card = e;
-//   // let scale = 1,
-//   //   rotate = 0;
-
-//   // if (i !== cards.length - 1) {
-//   //   scale = 0.9 + 0.025 * i;
-//   //   rotate = -10;
-//   // }
-
-//   gsap.to(card, {
-//     // scale: scale,
-//     // rotationX: rotate,
-//     // transformOrigin: "top center",
-//     // ease: "none",
-//     scrollTrigger: {
-//       trigger: e,
-//       // start: "top " + (70 + 40 * i) + "px",
-//       start: "top 500px",
-//       end: "bottom -100px",
-//       endTrigger: ".end-anim",
-//       pin: e,
-//       pinSpacing: false,
-//       scrub: true,
-//       markers: true,
-//     },
-//   });
-// });
-
-// gsap.to("#card1", {
-//   scrollTrigger: {
-//     trigger: ".cards-wrapper",
-//     start: "top top",
-//     end: "bottom bottom",
-//     pin: "#card1",
-//     markers: true,
-//     scrub: false,
-//     toggleActions: "play none none reverse",
-//   },
-//   y: -700,
-//   duration: 1,
-//   ease: "none",
-// });
+    const target = document.querySelector(this.getAttribute("href"));
+    if (target) {
+      lenis.scrollTo(target);
+    }
+  });
+});
 
 // blobs animation
 document.addEventListener("DOMContentLoaded", () => {
